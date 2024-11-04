4dcf94865c33a7145569e46db563c3a808921867
Skirynmax <jose.alban.montoya@colexio-karbo.com>
2024-11-04 17:10:25 +0100

    mi oommit

---
+++ src/main/java/com/mycompany/operaciones/Operaciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operaciones;
+
+/**
+ *
+ * @author alban
+ */
+public class Operaciones {
+
+    public static void main(String[] args) {
+      
+    }
+}

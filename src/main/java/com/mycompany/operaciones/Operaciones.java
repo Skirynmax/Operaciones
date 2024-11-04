e51a4c212afed601b6d6fb20850d56bb2cf9fd6c
Skirynmax <jose.alban.montoya@colexio-karbo.com>
2024-11-04 17:57:23 +0100

    mi oommit

--- src/main/java/com/mycompany/operaciones/Operaciones.java
+++ src/main/java/com/mycompany/operaciones/Operaciones.java
@@ -8,9 +8,67 @@ package com.mycompany.operaciones;
  *
  * @author alban
  */
+
+import java.util.Scanner;
+
 public class Operaciones {
 
+   
+    
     public static void main(String[] args) {
+      	
+		
+        System.out.println("Selecciona operación \n 1.Resta \n 2.Suma \n 3.Multiplicación");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+       int operator = scanner.nextInt();
+        
+       
+       
+        while(true){
+             
+            
+            if(operator == 1){
+                
+                 System.out.println("Usted ha elegido resta \n Indica una cifra");
+                 int number1 =(int) scanner.nextInt();
+                 
+                 System.out.println("Indica otra cifra");
+                 int number2= (int) scanner.nextInt();
+                 
+                 
+                 //aqui meted la resta
+            }
+            if(operator == 2){
+                
+                 System.out.println("Usted ha elegido suma \n Indica una cifra");
+                 int number1 =(int) scanner.nextInt();
+                 
+                 System.out.println("Indica otra cifra");
+                 int number2= (int) scanner.nextInt();
+                 
+                 //aqui meted la suma
+            }
+            if(operator == 3){
+                
+                 System.out.println("Usted ha elegido multiplicación \n Indica una cifra");
+                 int number1 =(int) scanner.nextInt();
+                 
+                 System.out.println("Indica otra cifra");
+                 int number2= (int) scanner.nextInt();
+                 
+                 //aqui meted la multi
+            }
+           
+		
+            
+        }
       
+			
+		
+			
+                        
+		
     }
 }
